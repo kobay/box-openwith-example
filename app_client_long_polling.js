ba1899d08eec01693320430c254684bfbb615cc8
hiroyuki kobayashi <hkobayashi@box.com>
2020-05-20 10:20:31 +0900

    wip

--- app_client_long_polling.js
+++ app_client_long_polling.js
@@ -16,12 +16,10 @@ const FILE_ID = "665319803554";
 
 app.get("/", async (req, res) => {
   try {
-    console.log("config", config);
     const sdk = boxSDK.getPreconfiguredInstance(config);
     // AppUserの権限でClientオブジェクトを作成
     const auClient = sdk.getAppAuthClient("user", USER_ID);
 
-    console.log(2);
     // トークンをダウンスコープする
     // APIリファレンスには載っていないが、UI Elementsの説明には書いてあるAPI
     // ここでは、OpenWithで必要なものと、Previewで必要なものを両方スコープにいれてトークンをダウンスコープする
@@ -35,13 +33,11 @@ app.get("/", async (req, res) => {
       `https://api.box.com/2.0/folders/0`
     );
 
-    console.log(3);
     // テンプレートにパラメータを渡して、HTMLを返す
     res.render("index_client_long_polling", {
       fileId: FILE_ID,
       token: downToken.accessToken,
     });
-    console.log(4);
   } catch (e) {
     console.error(e.toString());
   }
