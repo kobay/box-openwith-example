29840c6f97d499b88014693c55073632803ca06d
hiroyuki kobayashi <hkobayashi@box.com>
2020-05-20 10:17:55 +0900

    wip

--- config.js
+++ config.js
@@ -3,9 +3,11 @@ require("dotenv").config();
 const clientID = process.env.clientID;
 const clientSecret = process.env.clientSecret;
 const publicKeyID = process.env.publicKeyID;
-const privateKey = process.env.privateKey;
 const passphrase = process.env.passphrase;
 const enterpriseID = process.env.enterpriseID;
+
+// privateKeyには\nが含まれているので、これを改行に変換する
+const privateKey = process.env.privateKey.replace(/\\n/gm, "\n");
 
 module.exports = {
   boxAppSettings: {
